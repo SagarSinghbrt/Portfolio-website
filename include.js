b832687ddf341b6ecd8294fec67e508b3809b638
SagarSinghbrt <biratnagar14morang@gmail.com>
2025-06-06 03:00:45 +0545

    debugging

--- include.js
+++ include.js
@@ -1,35 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-	// Determine base path for fetch depending on current page location
-	const basePath = window.location.pathname.includes("/html/")
-		? "../html/"
-		: "html/";
+const basePath = "/html/";
 
-	fetch(basePath + "header.html")
-		.then((res) => res.text())
-		.then((data) => {
-			const header = document.querySelector("header");
-			if (header) {
-				header.innerHTML = data;
-				document.dispatchEvent(new Event("headerLoaded"));
-			}
-		});
+fetch(basePath + "header.html")
+	.then((res) => res.text())
+	.then((data) => {
+		const header = document.querySelector("header");
+		if (header) header.innerHTML = data;
+	});
 
-	fetch(basePath + "footer.html")
-		.then((res) => res.text())
-		.then((data) => {
-			const footer = document.querySelector("footer");
-			if (footer) {
-				footer.innerHTML = data;
-			}
-		});
+fetch(basePath + "footer.html")
+	.then((res) => res.text())
+	.then((data) => {
+		const footer = document.querySelector("footer");
+		if (footer) footer.innerHTML = data;
+	});
 
-	fetch(basePath + "qnsAns.html")
-		.then((res) => res.text())
-		.then((data) => {
-			const qnsAnsSection = document.querySelector(".qnsAns-section");
-			if (qnsAnsSection) {
-				qnsAnsSection.innerHTML = data;
-				document.dispatchEvent(new Event("qnsAnsLoaded"));
-			}
-		});
-});
+fetch(basePath + "qnsAns.html")
+	.then((res) => res.text())
+	.then((data) => {
+		const qnsAnsSection = document.querySelector(".qnsAns-section");
+		if (qnsAnsSection) qnsAnsSection.innerHTML = data;
+	});
